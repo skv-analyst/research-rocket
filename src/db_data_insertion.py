4a582acac9683cee7f33ca62e250c336c409e167
konstantin-samsonov <skv.inet@gmail.com>
2024-05-19 23:04:54 +0300

    Add scripts and data

---
+++ src/db_data_insertion.py
@@ -0,0 +1,36 @@
+import os
+from pony.orm import db_session
+from datetime import datetime
+from src.db_models import Project, Interview, db
+
+
+@db_session
+def add_interviews_from_folder(project_name, folder_path):
+    with db_session:
+        # Adding a project or finding an existing one
+        project = Project.get(name=project_name)
+        if not project:
+            project = Project(name=project_name)
+
+        # We read everything .txt files from the specified folder
+        for filename in os.listdir(folder_path):
+            if filename.endswith('.txt'):
+                file_path = os.path.join(folder_path, filename)
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    content = file.read()
+
+                    # Extract the date from the file name (the format is assumed YYYY-MM-DD.txt )
+                    # date_str = filename.split('.')[0]
+                    # interview_date = datetime.strptime(date_str, '%Y-%m-%d')
+
+                    # Adding interviews to the database
+                    Interview(project=project, content=content)
+
+        # Saving changes to the database
+        db.commit()
+
+
+if __name__ == '__main__':
+    project_name = 'test'
+    folder_path = '../data/raw/test_interviews'
+    add_interviews_from_folder(project_name, folder_path)
---
+++ src/db_models.py
@@ -0,0 +1,44 @@
+from pony.orm import Database, Required, Set, Optional, PrimaryKey
+from datetime import datetime
+
+# Creating a database object
+db = Database()
+
+
+# Модель проекта
+class Project(db.Entity):
+    _table_ = "projects"
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    interviews = Set('Interview')  # One project can have many interviews
+
+
+# Модель интервью
+class Interview(db.Entity):
+    _table_ = "interviews"
+    id = PrimaryKey(int, auto=True)
+    project = Required(Project)  # Each interview is related to one project
+    date = Optional(datetime)  # Date of the interview (optional)
+    content = Required(str)  # Interview Content
+    llm_answers = Set('LLMAnswer')  # One interview can have many answers
+
+
+# Модель ответа на вопросы
+class LLMAnswer(db.Entity):
+    _table_ = "llm_answers"
+    id = PrimaryKey(int, auto=True)
+    interview = Required(Interview)  # Each answer is associated with one interview
+    created = Required(datetime, default=datetime.now)  # Date and time the response was created
+    model = Required(str)  # Name of the LLM model
+    input_tokens = Required(int)  # Number of input tokens
+    output_tokens = Required(int)  # Number of output tokens
+    prompt = Required(str)  # Request text
+    answer_full = Required(str)  # Full text of the response
+    answer_clear = Required(str)  # Cleared and short response text
+
+
+# Connecting to the SQLite database
+db.bind('sqlite', '../data/db.sqlite', create_db=True)
+
+# Generating tables in the database
+db.generate_mapping(create_tables=True)
