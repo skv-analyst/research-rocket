9f61d139b26b698c7e3030a718f565a5819d476d
konstantin-samsonov <skv.inet@gmail.com>
2024-05-22 23:04:39 +0300

    Add scripts and data

--- src/data/models.py
+++ src/data/models.py
@@ -1,5 +1,5 @@
-from pony.orm import Database, Required, Set, Optional, PrimaryKey
 from datetime import datetime
+from pony.orm import Database, PrimaryKey, Required, Set, Optional, Json
 from src.paths import PATH_TO_DATA
 
 # Creating a database object
@@ -11,32 +11,33 @@ class Project(db.Entity):
     _table_ = "projects"
     id = PrimaryKey(int, auto=True)
     name = Required(str)
-    interviews = Set('Interview')  # One project can have many interviews
-
+    interviews = Set('Interview')                       # One project can have many interviews
+    llm_answers = Set('LLMAnswer')                      # One project can have many llm_answer
 
 # Модель интервью
 class Interview(db.Entity):
     _table_ = "interviews"
     id = PrimaryKey(int, auto=True)
-    project = Required(Project)  # Each interview is related to one project
-    date = Optional(datetime)  # Date of the interview (optional)
-    content = Required(str)  # Interview Content
-    llm_answers = Set('LLMAnswer')  # One interview can have many answers
+    project = Required(Project)                         # Each interview is related to one project
+    date = Optional(datetime)                           # Date of the interview (optional)
+    content = Required(str)                             # Interview Content
+    llm_answers = Set('LLMAnswer')                      # One interview can have many answers
 
 
 # Модель ответа на вопросы
 class LLMAnswer(db.Entity):
     _table_ = "llm_answers"
     id = PrimaryKey(int, auto=True)
-    interview = Required(Interview)  # Each answer is associated with one interview
-    analysis_step = Required(int)   # The interview analysis step
+    project = Required(Project)                       # Each answer is associated with one project
+    interview = Required(Interview)                     # Each answer is associated with one interview
+    analysis_step = Required(int)                       # The interview analysis step
     created = Required(datetime, default=datetime.now)  # Date and time the response was created
-    model = Required(str)  # Name of the LLM model
-    input_tokens = Required(int)  # Number of input tokens
-    output_tokens = Required(int)  # Number of output tokens
-    prompt = Required(str)  # Request text
-    answer_full = Required(str)  # Full text of the response
-    answer_clear = Required(str)  # Cleared and short response text
+    model = Required(str)                               # Name of the LLM model
+    input_tokens = Required(int)                        # Number of input tokens
+    output_tokens = Required(int)                       # Number of output tokens
+    prompt = Required(str)                              # Request text
+    answer_full = Required(str)                         # Full text of the response
+    answer_clear = Required(Json)                       # Cleared and short response text
 
 
 # Connecting to the SQLite database
