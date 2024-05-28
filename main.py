78cf11bd0b3c4f98f4318a007be11bdf85f8aaf0
konstantin-samsonov <skv.inet@gmail.com>
2024-05-28 22:19:53 +0300

    Add scripts and data

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-from src.data import working_db_2 as DATABASE
 
-
-
+from src.data import working_db_2 as db
+from src.llm.prompt_templates import INTERVIEW_QUESTIONS, SUMMARY_QUESTIONS
+from src.llm import working_llm_2 as llm2
+from src.etl import working_text as etl
 
 def main(project_name: str = None, full_pipeline: bool = False) -> None:
     if full_pipeline:
-        DATABASE.save_to_db(table_name="projects", name=project_name)
-        DATABASE.save_files_to_db(project_name=project_name)
+        db.save_to_db(table_name="projects", name=project_name)
+        db.save_files_to_db(project_name=project_name)
 
-    # print(f"Started processing the interview")
-    # ProcessAllInterviewsProject(project_name, questions=INTERVIEW_QUESTIONS).run()
-    # print(f"The start of summarization")
-    # ProcessResultsAllInterviews(project_name=project_name, questions=SUMMARY_QUESTIONS).run()
-    # print("Saving the results to a file")
-    # str_merged, str_summarize = CreateSTR2PDF(project_name=project_name).run()
-    # save_string_to_pdf(text=str_summarize, path_to_save=folder_path, filename="ouput_summary", line_length=70)
+
+    # Обрабатываем каждое интервью проекта
+    # llm2.LlmProcessInterview(project_name=project_name, questions=INTERVIEW_QUESTIONS).run()
+
+    # Получаем строку с результатами обработки всех интервью проекта
+    all_llm_answer_interviews = etl.PrepareInterviewForSummary(project_name=project_name).run()
+
+    # Запускаем LlmProcessProject
+
+    # Отдельный модуль подготовки данных для pdf и сбора pdf
+
+
 
 
 if __name__ == "__main__":
-    name = "test_real_estate_4"
+    name = "test_real_estate_2"
     main(project_name=name, full_pipeline=False)
